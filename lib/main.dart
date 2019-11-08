636e5b372c796c02f3eb37dcc0ab313603a29f50
pocari <caffelattenonsugar@gmail.com>
2019-11-08 09:05:24 +0900

    ウィジェット自身で状態管理するパターン

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+        title: 'Navigation',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('TapBox'),
+          ),
+          body: Center(child: TapBoxA()),
+        ),
+      ),
+    );
 
-class MyApp extends StatelessWidget {}
+// ウィジェット自身で状態管理するパターン
+class TapBoxA extends StatefulWidget {
+  _TapBoxAState createState() => _TapBoxAState();
+}
+
+class _TapBoxAState extends State<TapBoxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: _handleTap,
+        child: Container(
+          child: Center(
+            child: Text(
+              _active ? 'Active' : 'Inactive',
+              style: TextStyle(fontSize: 32.0, color: Colors.white),
+            ),
+          ),
+          width: 200.0,
+          height: 200.0,
+          decoration: BoxDecoration(
+            color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+          ),
+        ),
+      );
+}
